f179761ee345c674d133667851dc39c247eaca07
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-03 21:20:05 +0200

    added store module to organize better a structure of the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-import { createStore} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers'
-
-import { actorsActions } from './app/actors/duck'
-import { moviesActions } from './app/movies/duck'
-
-const store = createStore(rootReducer, composeWithDevTools());
-window.store = store;
-
-store.dispatch(actorsActions.add('Brad Pitt'));
-store.dispatch(moviesActions.add('The Intern'));
-
 function App() {
   return (
     <div className="App">
