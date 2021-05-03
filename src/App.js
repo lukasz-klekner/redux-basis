e223409166d34a1a5e2d25438530957267a5a88a
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-03 12:50:41 +0200

    added ADD and RESET actions into reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
+import { createStore } from 'redux';
+
 import './App.css';
+
+
+const initialMovies = {
+  listName : 'favourite',
+  movies: [
+    'Titcanic', 'Dijango', 'Rambo'
+  ]
+}
+
+function movies(state = initialMovies, action) {
+  switch(action.type){
+    case 'RESET':
+      return {
+        ...state, movies: [],
+      }
+
+    case 'ADD':
+      return {
+        ...state, movies: [...state.movies, action.movie]
+      }
+
+    default:
+    return state;
+  }
+}
+
+const store = createStore(movies);
+
+window.store = store;
+
+// store.getState()
+// store.dispatch({type: "RESET"})
+// store.dispatch({type: "ADD", movie: 'Szybcy i Wsciekli'})
 
 function App() {
   return (
